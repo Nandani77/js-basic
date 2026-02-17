ca979573e4f71106865053adefbdefc2caed1522
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-02-17 11:34:55 +0000

    type os  complete

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("nandani")
+// console.log("nandani")
+let score = "33";
+// console.log(typeof (score));
+let ValueInNumber = Number(score);
+// console.log(typeof (score));
+// console.log(typeof (ValueInNumber));
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof someNumber);
