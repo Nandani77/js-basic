a17d2457dfcc14a8e497f3b2348651e1e71cf67e
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-02-17 12:51:49 +0000

    data type

---
+++ 01_basics/summery.js
@@ -0,0 +1,35 @@
+// data types - primitive + non primitive 
+
+// 1 primitive
+
+// 7 types - call by value - string, num, boolean , null,undefined, symbol, bigInt
+
+// refrence type / non-primitive - array , objects, functions
+
+
+// const score = 100;
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bigNumber = 103495878903948893n
+
+
+// non - primitive
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("hii nanadnai");
+}
+console.log(typeof bigNumber)
+console.log(typeof heros)
+console.log(typeof myFunction)
