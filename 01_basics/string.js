c6d4dfbbb8c78287b2f6a3d5fedd86a96432b729
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-02-22 11:27:00 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -2,7 +2,7 @@ const name = "nandani"
 const count = 1;
 // console.log(name + count + "value");
 // console.log(`hallo mu name is ${name} and my count is ${count}`);
-const gameName = new String('nandnait');
+const gameName = new String('nandnait-cr-pt');
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 // console.log(gameName.length);
@@ -12,4 +12,14 @@ const gameName = new String('nandnait');
 
 const newString = gameName.substring(0,4)
 console.log(newString);
+const anotherString = gameName.slice(-8, 5)
+console.log(anotherString);
 
+const newStringOne = "   Nandani   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://nandani.com/nandani%20singh"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sunder'))
+console.log(gameName.split('-'))
