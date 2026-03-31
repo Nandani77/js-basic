8c39fdee4474f99c2d76e244990e867671440f5b
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-03-31 02:18:43 +0000

    array methods

--- 02_arrays/01-arrays.js
+++ 02_arrays/01-arrays.js
@@ -1,5 +1,35 @@
  const myArr = [0, 1,2,3,4,5,6,7,8];
- console.log(myArr[8  ]);
+//  console.log(myArr[8  ]);
 
  const myHeros = ["hero1", "hero12"];
- console.log(myHeros[1]);
+//  console.log(myHeros[1]);
+ // aaray methodes
+
+
+//  myArr.push(6)
+//  myArr.push(7)
+//  myArr.pop(7)
+//  myArr.unshift(9)
+//  myArr.shift()
+
+
+//  console.log(myArr)
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+// console.log( newArr);
+
+
+// slice, splice
+const myn1 = myArr.slice(1, 4);
+console.log(myn1)
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 4)
+console.log("C", myArr);
+
+console.log(myn2);
