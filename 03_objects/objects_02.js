8cd0c0baf60c0aaad62dfbc6da57ae45abe1db38
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-07 08:43:52 +0000

    object-2

---
+++ 03_objects/objects_02.js
@@ -0,0 +1,16 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123qwe"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+console.log(tinderUser);
+
+const regularUser= {
+    email: "nandani@323@gmail.com",
+    fullname: {
+        userFullName :{
+            firstname: "nandani",
+            lastname: "Singh   "
+        }
+    }
+}
