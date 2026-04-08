2f84930dadd6be4daf2abbb458a4f7d172b8e07e
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-08 07:51:32 +0000

    object methos

--- 03_objects/objects_02.js
+++ 03_objects/objects_02.js
@@ -14,3 +14,40 @@ const regularUser= {
         }
     }
 }
+
+// console.log(regularUser.fullname.userFullName.firstname);
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj4 = {5: "a", 6: "b"};
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+
+// console.log(obj3)
+
+
+const users = [
+    {
+    id: 1,
+    email: "n@gmail.com"
+   },
+   {
+    id: 2,
+    email: "n@gmail.com"
+    
+   },
+   {
+    id: 3,
+    email: "n@gmail.com"
+   },
+
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
