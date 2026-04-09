e29db20fbb87a12a49cbd1d244d68cf869b55de7
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-09 09:22:37 +0000

    array in to function

--- 04_functions/functions_01.js
+++ 04_functions/functions_01.js
@@ -37,16 +37,40 @@ function LoginUserMessage(username = "sam"){
     // }  
     // both equivalent to each other 
        if(!username){
-        console.log("please enter the a username");
+        //console.log("please enter the a username");
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(LoginUserMessage("real things"));
-console.log(LoginUserMessage("nandani"));
+// console.log(LoginUserMessage("nandani"));
 
+function calculateCarPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCarPrice(200, 300, 400, 2000))
+ const user ={
+    username: "ouvvyb",
+    prices: 199
+ }
 
+ function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+ }
+
+//  handleObject(user)
+handleObject({
+    username: "sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue(myNewArray));
 
 
 
