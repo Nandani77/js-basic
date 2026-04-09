0c34e69f84ca27af04fa7d063ca98dbbfe055c6a
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-09 10:49:31 +0000

    function scope

--- 04_functions/functions_02.js
+++ 04_functions/functions_02.js
@@ -8,7 +8,7 @@ let a = 10
 const b = 20
 //var c = 30
 
-console.log("INNER: ", a);
+// console.log("INNER: ", a);
 }
 
 for(let i = 0; i<Array.length; i++){
@@ -16,6 +16,49 @@ for(let i = 0; i<Array.length; i++){
 }
 
 
- console.log(a);
+//  console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// nested score + closer
+
+function one(){
+    const username = "nandani"
+
+    function two(){
+        const website = "teckdude"
+        console.log(username)
+    }
+            // console.log(website);
+two()
+
+}
+// one()
+
+
+if(true){
+    const username = "nandu"
+
+    if(username==="nandu"){
+        const website = "  youtube"
+        console.log(username + website);
+    }
+
+    // console.log(website)
+}
+
+// console.log(username);
+
+
+
+// intrusting
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
