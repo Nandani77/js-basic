ac781cea36c8c3abbd570fbcac29ef782eaaf29c
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-12 18:32:27 +0000

    if else

---
+++ 05_control_flow/one.js
@@ -0,0 +1,56 @@
+// if
+
+// const isUserLogedin = true
+// const temperature = 41
+
+// if (temperature <50){
+// console.log("lesss than 50");
+// }
+
+// else{
+//     console.log("Greater than 50");
+
+// }
+
+// if (2!=3){
+// console.log("executed");
+// }
+
+
+
+
+// const score = 200;
+// if(score >100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`user Power: ${power}`);
+
+
+
+// const balance = 2000;
+// if(balance <555){
+//  console.log("less than 555");
+// } else if(balance<780){
+//     console.log("lesss than 780");
+// } else if(balance<880){
+//     console.log("lesss than 880");
+// } else {
+//     console.log("less than 1000");
+// }
+
+const isUserLogedin = true
+const debitcard = true
+const googlelogedinj = true
+
+const Emaillogin = true
+
+if(isUserLogedin && debitcard  && 2==3){
+    console.log("allow to by the course")
+}
+
+if(googlelogedinj || Emaillogin){
+    console.log("User Loggged In");
+    
+}
