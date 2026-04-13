952c21d8977b9912bf8863bdaf543719a1799283
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-13 02:49:18 +0000

    inner and outer loop

---
+++ 06_loops/loops_01.js
@@ -0,0 +1,16 @@
+for(let a =0; a<=10;a++){
+
+    const element = a;
+    if (element == 5){
+      //console.log("5 is best number");
+    }
+  //console.log(element);
+}
+
+for(let a =0; a<=10;a++){
+console.log (`Outer loop value: ${a}` );
+for (let j = 0; a<= 10; j++ ){
+console.log(`Inner loop value &{j} and  Inner loop ${a}`);
+}
+
+}
