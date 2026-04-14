95624e02265a179fe60f36208a4b92391d9de280
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-14 02:06:03 +0000

    for in loop

---
+++ 06_loops/for.js
@@ -0,0 +1,14 @@
+const myobj = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby', 
+}
+
+for (const key in myobj) {
+   console.log(myobj[key])
+    
+}
+
+
+// for in for keys 
+// for of loop for values
