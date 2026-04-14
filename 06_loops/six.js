df2b901fb6272167c28cdb0d4e5b152dfd99c322
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-14 08:25:45 +0000

    filter

---
+++ 06_loops/six.js
@@ -0,0 +1,14 @@
+const coding = ["js", "ruby", "java", "python"]
+const values = coding.forEach((item, values)=>{
+    // console.log(item);
+    // return item
+})
+
+
+// console.log(values);
+// return values;
+
+const mynums = [1,2,3,4,5,6,7]
+
+const newNums = mynums.filter( (num) => 4)
+console.log(newNums);
