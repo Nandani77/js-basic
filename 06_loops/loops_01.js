26e3a6dd15a5e23a74c6bfa85a4a217c77b9ae4f
Raj Nandani <92445543+Nandani77@users.noreply.github.com>
2026-04-14 01:15:36 +0000

    break and continue

--- 06_loops/loops_01.js
+++ 06_loops/loops_01.js
@@ -8,9 +8,33 @@ for(let a =0; a<=10;a++){
 }
 
 for(let a =0; a<=10;a++){
-console.log (`Outer loop value: ${a}` );
-for (let j = 0; a<= 10; j++ ){
-console.log(`Inner loop value &{j} and  Inner loop ${a}`);
+// console.log (`Outer loop value: ${a}` );
+for (let j = 0; j<= 10; j++ ){
+// console.log(`Inner loop value &{j} and  Inner loop ${a}`);
+
+// console.log(a + '*' + j + ' = ' + a*j );
 }
 
+}
+
+
+let myArray = ['flash', 'batman', 'superman']
+
+// console.log(myArray.length);
+for( let i=0; i<myArray.length; i++){
+  const j = myArray[i];
+  // console.log(j);
+}
+
+// BREAK AND CONTINUE
+
+
+for( let i=1; i<=20; i++){
+  if(i==5){
+    console.log(`Detected 5`);
+    continue;
+  }
+
+
+  console.log(i);
 }
